13b63775acce14ed5d8b33994e770561f79fbd90
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2019-11-07 01:04:26 -0300

    Primeira Versão jogo da velha 07/11/2019

---
+++ game.js
@@ -0,0 +1,76 @@
+const player1 = "X";
+const player2 = "O";
+var playTime = player1;
+var gameOver = false;
+
+atualizaMostrador();
+inicializarEspacos();
+
+function atualizaMostrador(){
+    if(gameOver){return;}
+    if(playTime == player1){
+        var player = document.querySelectorAll("div#mostrante img")[0];
+        player.setAttribute("src", "imagens/letterX.png")
+    }else{
+        var player = document.querySelectorAll("div#mostrante img")[0];
+        player.setAttribute("src", "imagens/circle.png")
+    }
+}
+
+function inicializarEspacos(){
+    var espacos = document.getElementsByClassName("espaco");
+    for(var i = 0; i < espacos.length; i++){
+        espacos[i].addEventListener("click", function(){
+            if(gameOver){return;}
+            if(this.getElementsByTagName("img").length == 0){
+                if(playTime == player1){
+                    this.innerHTML = "<img src='imagens/letterX.png'>";
+                    this.setAttribute("jogada", player1);
+                    playTime = player2;
+                }else{
+                    this.innerHTML = "<img src='imagens/circle.png'>";
+                    this.setAttribute("jogada", player2);
+                    playTime = player1;
+                }
+                atualizaMostrador();
+                verificaVencedor();
+            }
+        });
+    }
+}
+
+async function verificaVencedor(){
+    var a1 = document.getElementById("a1").getAttribute("jogada")
+    var a2 = document.getElementById("a2").getAttribute("jogada")
+    var a3 = document.getElementById("a3").getAttribute("jogada")
+
+    var b1 = document.getElementById("b1").getAttribute("jogada")
+    var b2 = document.getElementById("b2").getAttribute("jogada")
+    var b3 = document.getElementById("b3").getAttribute("jogada")
+
+    var c1 = document.getElementById("c1").getAttribute("jogada")
+    var c2 = document.getElementById("c2").getAttribute("jogada")
+    var c3 = document.getElementById("c3").getAttribute("jogada")
+
+    var vencedor = "";
+
+    if(((a1 == b1 && a1 == c1 )|| (a1 == a2 && a1 == a3) || (a1 == b1 && a1 == c3)) && a1 != ""){
+        vencedor = a1;
+    }else if(((b2 == b1 && b2 == b3 )|| (b2 == a2 && b2 == c2) || (b2 == a1 && b2 == c3)) && b2 != ""){
+        vencedor = b2;
+    }else if(((c1 == a1 && c1 == b1 )|| (c1 == c2 && c1 == c3) || (c3 == b2 && c3 == a1)) && a1 != ""){
+        vencedor = c3;
+    }    
+
+    if(vencedor != ""){
+        gameOver = true;
+
+        await sleep(50);
+        alert("O vencedor é : '" + vencedor + "'")
+    }
+
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
